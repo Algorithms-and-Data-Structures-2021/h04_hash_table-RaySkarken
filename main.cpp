f9140dd78d0cc5a24f62f30db4a2c9104beaeec0
Orkhan Kazymov <rayskarken@gmail.com>
2021-05-11 23:32:39 +0300

    Did homework, I guess 0_+

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
 
 int main(int argc, char **argv) {
-  std::cout << "Hello, stranger!" << std::endl;
-  return 0;
+    std::vector<int> a = *new std::vector<int>[10];
+
+    std::cout << a.size() << std::endl;
+    std::cout << a.capacity() << std::endl;
+    a.resize(15);
+    std::cout << a.size() << std::endl;
+    std::cout << a.capacity() << std::endl;
+
+    a.push_back(1);
+    a.push_back(2);
+    std::cout << a.size() << std::endl;
+    std::cout << a.capacity() << std::endl;
+
+    std::cout << a[3] << std::endl;
+
+    return 0;
 }
